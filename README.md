38cce1e0932fb4fea71e97721b8bd6006c747ab8
RaymondW9 <60833199+RaymondW9@users.noreply.github.com>
2020-04-08 18:48:48 -0400

    Added web interaction

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Spot
- SMS bot for financial information
+ SMS bot for financial information Using pythonanywhere to host a site and Twilio webhooks to recieve inbound messages.
--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 from twilio.twiml.messaging_response import MessagingResponse
 import requests
 from bs4 import BeautifulSoup
@@ -6,12 +6,21 @@ import retrieve_data
 
 app = Flask(__name__)
 
-#Test if web app is working
+#Home page with ticker lookup
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask!'
+    return render_template('search_ticker.html')
 
-#Get current stock price for the given ticker symbol by scraping yahoo finance
+@app.route('/search')
+def search():
+
+    name = request.args.get('name')
+    stock = get_current_price(name)[1]
+    price = get_current_price(name)[2]
+
+    return render_template('stock_price.html', stock=stock, price=price)
+
+#Get current stock price for the given ticker symbol
 def get_current_price(ticker):
     path = "https://finance.yahoo.com/quote/{0}/"
     path = path.format(ticker)
@@ -19,31 +28,28 @@ def get_current_price(ticker):
     bs = BeautifulSoup(result.text, 'html.parser')
     try:
         title = bs.find('title').contents[0]
+        stock = title.split(" (", 1)[0]
         price = bs.find('span', 'Trsdu(0.3s) Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(b)').contents[0]
     except:
-        return 'Sorry, I can\'t find that stock. Check if your ticker symbol is correct and format your text like this: ticker GOOG'
-    return str(title.split(" (", 1)[0] + ': ' + price)
+        return 'Sorry, I can\'t find that stock. Check if your ticker symbol is correct and format your text like this: \nticker GOOG'
+    return [str(stock + ': ' + price), stock, price]
 
 @app.route('/sms', methods=['GET', 'POST'])
 def sms_reply():
-    #Parse text message
     resp = MessagingResponse()
     body = request.values.get('Body', None)
     command = str(body).lower()
-    
-    if command == 'hello':
+
+    if command == 'hello' or command == 'hi':
         resp.message('Hi there. My name is Spot. I\'m an sms bot that can get real-time and historical financial data for you. To get a list of commands, text \'commands\'.')
     elif command == 'commands':
-        resp.message('''Commands:
-            For current stock prices, text: 
-            ticker {SYMBOL}
-            Example: ticker GOOG
-            
-            More features coming soon.
-            ''')
+        resp.message('''Commands: \n
+        --------------------\nFor current stock prices, text: \nticker {SYMBOL} \nExample: ticker GOOG
+        --------------------
+        More features coming soon.''')
     elif 'ticker' in command:
         ticker = command[7:]
-        price = get_current_price(ticker)
+        price = get_current_price(ticker)[0]
         resp.message(price)
     else:
         resp.message('I\'m sorry, I don\'t understand your request. Text \'hello\' for an introduction or \'commands\' for a list of commands.')
